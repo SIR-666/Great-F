f07f6593f0b5fa474b5df5f6460873378e7c81a1
alqoroni <fathurrhazaq@gmail.com>
2025-07-18 14:10:46 +0700

    feat: update button text to 'Kembali' and add exam-time image asset

--- pages/training/dashboard-user/training-details/index.js
+++ pages/training/dashboard-user/training-details/index.js
@@ -15,7 +15,7 @@ const TrainingShow = () => {
                             <svg className="w-5 h-5 mr-2 group-hover:text-blue-600 transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                             </svg>
-                            Back to Dashboard
+                            Kembali
                         </button>
                     </div>
 
