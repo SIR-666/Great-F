878d162d7646e61e0306ca61c2e904b150dc5a14
alqoroni <fathurrhazaq@gmail.com>
2025-09-30 16:02:20 +0700

    fix: expose validateIdentity function in AuthProvider

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -333,6 +333,7 @@ export const AuthProvider = ({ children }) => {
         forgotPassword,
         resetPassword,
         getIdentityData,
+        validateIdentity,
       }}
     >
       {children}
