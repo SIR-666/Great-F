cc8f3955b82548377d9cac80ae88f2881834c376
alqoroni <fathurrhazaq@gmail.com>
2025-10-03 13:50:22 +0700

    feat: Update HomePage to use LayoutHomePage and include MooNewsList component

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
 import Link from "next/link";
-import Layout from "@/components/Layout";
+import Layout from "@/components/LayoutHomePage";
 import EventItem from "@/components/EventItem";
 import { API_URL } from "@/config/index";
+import Image from "next/image";
+import MooNewsList from "@/components/MooNewsList";
 
 export default function HomePage({ events }) {
   return (
     <Layout>
       <h1>Moo News</h1>
+      <MooNewsList />
+
+
       {/* {events.length === 0 && <h3>No projects to show</h3>}
 
       {events.map((evt) => (
